1fb7c5775bb8ee6f3d7e82c6187afc615d237295
AbdulHeisenberg <81732957+wdrtsptn@users.noreply.github.com>
2026-01-02 14:06:06 +0700

    Update pricing.js

--- pricing.js
+++ pricing.js
@@ -45,3 +45,15 @@ Thank you.`;
     }
   });
 });
+
+const toggleButons =
+  document.querySelectorAll('.toggle-btn');
+
+toggleButtons.forEach(btn => {
+  btn.addEventListener('click',() => {
+    const targetId = btn.dataset.target;
+    const content = document.getElementById(targetId);
+
+    content.classList.toggle('active');
+  });
+});
